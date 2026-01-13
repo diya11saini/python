2f1ddac7c99c4b718c1ec5ea638217a66dd50a3a
diya11saini <sainidiya100@gmail.com>
2026-01-13 12:28:26 +0000

    13-1-26

---
+++ sum.py
@@ -0,0 +1,12 @@
+p=eval(input("enter"))
+r=eval(input("enter"))
+t=eval(input("enter"))
+si=(p*r*t)/100
+print(si)
+
+
+#leap year
+#time convert into minute and second and take input in hours
+#f=c*9/5+32 take c as a input
+#area of triangle
+#enter the marks of 5 subjects and calculate the percentage
