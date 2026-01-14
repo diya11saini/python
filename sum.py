8061768274d07017bd811e7a10fdfd60edc712ee
diya11saini <sainidiya100@gmail.com>
2026-01-14 08:39:19 +0000

    14-1-26

--- sum.py
+++ sum.py
@@ -1,9 +1,10 @@
+'''
 p=eval(input("enter"))
 r=eval(input("enter"))
 t=eval(input("enter"))
 si=(p*r*t)/100
 print(si)
-
+'''
 
 #leap year
 #time convert into minute and second and take input in hours
@@ -12,5 +13,50 @@ print(si)
 #enter the marks of 5 subjects and calculate the percentage
 
 
-
+'''
 print("hello")
+'''
+
+'''
+yr=eval(input("enter the number : "))
+if yr%4==0 & (yr%400==0 | yr%100!=0):
+    print("leap year")
+else:
+    print("non leap year")
+'''
+
+
+'''
+hr=eval(input("enter the time : "))
+time1 =hr*60
+time2 =hr*3600 
+print(time1)
+print(time2)
+'''
+
+'''
+c=eval(input("enter the temp : "))
+f=(c*9/5)+32
+print(f)
+'''
+
+
+'''
+b=eval(input("enter th number : "))
+h=eval(input("enter the number : "))
+area=1/2*b*h
+print(int(area))
+'''
+
+
+'''
+a=eval(input("no.1 : "))
+b=eval(input("no.2 : "))
+c=eval(input("no.3 : "))
+d=eval(input("no.4 : "))
+e=eval(input("no.5 : "))
+sum=a+b+c+d+e
+total=500
+per=(sum/total)*100
+print(per)
+'''
