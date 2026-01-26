d0e4041f416c2cc58fd86cd3e5d500003f339fc1
diya11saini <sainidiya100@gmail.com>
2026-01-26 13:46:21 +0530

    123

--- mysql.sql
+++ mysql.sql
@@ -2,3 +2,24 @@ create employee table  and department table:
 insert data:
 aggregate function: min max sum avg count--> int datatype
 update , select , where , union (joins- left+right), alter
+
+  groupby,having
+Display each department and the average CGPA of students in that department.
+
+Show departments where the average CGPA is greater than 8.0.
+
+Find the number of students in each department.
+
+Display departments that have more than 2 students.
+
+Find the maximum CGPA in each department.
+
+Show only those departments where the maximum CGPA is greater than 9.0.
+
+Display departments and total students where the minimum CGPA is below 6.0.
+
+Find departments where the average CGPA is between 7.0 and 9.0.
+
+Show departments that have at least one student with CGPA = 9.5.
+
+Display department-wise average CGPA, but only for departments having more than 1 student.
